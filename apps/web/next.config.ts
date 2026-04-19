0d79ab9b201eac27d2152c0810a13639de2605f2
th1n <thinex@wp.pl>
2026-04-19 15:21:29 +0200

    chore: build fix

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   typedRoutes: true,
   reactCompiler: true,
-  output: "standalone"
+  output: "standalone",
+  turbopack: {
+    root: path.join(__dirname),
+  },
 };
 
 export default nextConfig;
