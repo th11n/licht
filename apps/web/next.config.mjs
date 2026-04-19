5a4ba78688d007613cb7f15ccf65fa44bc27a022
th1n <thinex@wp.pl>
2026-04-19 15:30:46 +0200

    chore: change ts to mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,11 +1,11 @@
-import type { NextConfig } from "next";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
+/** @type {import('next').NextConfig} */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   typedRoutes: true,
   reactCompiler: true,
   output: "standalone",
